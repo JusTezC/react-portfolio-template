23ed0ab5ec95bd5134c26f30a3d8448b22b30f47
Tech Geek <martezjmartez@outlook.com>
2025-08-06 15:24:43 -0500

    INITITAL COMMIT

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -13,15 +13,15 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/JusTezC" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="www.linkedin.com/in/justezc" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
+          <h1>Martez Cathey</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/JusTezC" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="www.linkedin.com/in/justezc" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
